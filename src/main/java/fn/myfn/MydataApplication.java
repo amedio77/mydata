319458bf97a1bb70d823b9200edf17e418218f44
amedio <amedio77@gmail.com>
2022-01-12 11:04:30 +0900

    ms-sql mybatis test

--- src/main/java/fn/myfn/MydataApplication.java
+++ src/main/java/fn/myfn/MydataApplication.java
@@ -1,27 +1,7 @@
 package fn.myfn;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
 
 
 @SpringBootApplication
--- src/main/java/fn/myfn/oauth/Auth.java
+++ src/main/java/fn/myfn/oauth/Auth.java
@@ -9,14 +9,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import fn.myfn.comm.Util;
-
 
 /**
  * Description : 인증 토큰 관리
